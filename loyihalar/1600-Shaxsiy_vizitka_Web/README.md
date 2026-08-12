137faf9fdd6fb6fd91f32b064eafe48e8a7f88ea
Xudoybergano <xudoyberdiyevmurodjon4@gmail.com>
2026-08-12 23:32:33 +0500

    Loyiha: Shaxsiy vizitka (Web) — 77.9% (Yaxshi)

---
+++ loyihalar/1600-Shaxsiy_vizitka_Web/README.md
@@ -0,0 +1,3 @@
+<h1>Qochqarov Yodgor</h1>
+<h3>Dasturchi</h3>
+<p>Men Aziz Academy platformasida HTML vs CSS texnologiyalarni o'rganyapman.</p>
---
+++ loyihalar/1600-Shaxsiy_vizitka_Web/main.py
@@ -0,0 +1,3 @@
+<h1>Ismingiz va Familiyangiz</h1>
+<h3>Kasbingiz (masalan: Dasturchi)</h3>
+<p>O'zingiz haqingizda qisqacha qiziqarli matn.</p>
