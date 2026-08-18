e65fc8ccd77914c81bdea2b902bd9b89146cef5d
Xudoybergano <xudoyberdiyevmurodjon4@gmail.com>
2026-08-18 21:05:21 +0500

    Add: 15) Yakuniy: global counter + global log birga [Xudoybergano]

---
+++ lms_kod_topshiriqlari/06-Funksiyalar/05-Scope_____local_vs_global__global_kalit_so_zi/15-15__Yakuniy__global_counter___global_log_birga.py
@@ -0,0 +1,15 @@
+counter = 0
+logs = []
+
+def add(msg):
+    global counter
+    logs.append(msg)
+    counter += 1
+    
+def stats():
+    return f"count={counter}, logs={len(logs)}"
+
+n = int(input())
+for i in range(n):
+    add(input())
+print(stats())
