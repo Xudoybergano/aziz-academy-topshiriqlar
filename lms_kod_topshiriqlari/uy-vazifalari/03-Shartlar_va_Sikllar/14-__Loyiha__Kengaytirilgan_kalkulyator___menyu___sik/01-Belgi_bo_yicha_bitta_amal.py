68d3f45b3e174829e5cec7d8576c60e5f4c87098
Xudoybergano <xudoyberdiyevmurodjon4@gmail.com>
2026-08-19 11:09:01 +0500

    Add: Belgi bo'yicha bitta amal [Xudoybergano]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Belgi_bo_yicha_bitta_amal.py
@@ -0,0 +1,12 @@
+a = int(input())
+amal = input().strip()
+b = int(input())
+
+if amal == "+":
+    print(a + b)
+elif amal == "-":
+    print(a - b)
+elif amal == "*":
+    print(a * b)
+else:
+    print("Noma'lum amal")
