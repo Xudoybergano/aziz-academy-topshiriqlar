c558f372ba49e77a18f4cdfa874037f965167b2c
Xudoybergano <xudoyberdiyevmurodjon4@gmail.com>
2026-08-23 16:55:36 +0500

    Add: Vaqt konvertori menyu [Xudoybergano]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/03-__Shartli_menyu___oddiy_CLI_menyu/01-Vaqt_konvertori_menyu.py
@@ -0,0 +1,13 @@
+menu = int(input())
+n = int(input())
+
+if menu == 1:
+    minut = n // 60
+    soniya = n % 60
+    print(f"{minut} minut {soniya} soniya")
+elif menu == 2:
+    soat = n // 60
+    minut = n % 60
+    print(f"{soat} soat {minut} minut")
+else:
+    print("Notogri tanlov")
